451d1890a53b0d8029b840eeebe914fa7b436690
kwokkan <kwokkan@users.noreply.github.com>
2020-05-16 21:36:49 +0100

    Update layout with sidebar.

--- dist/src.62c16509.js
+++ dist/src.62c16509.js
@@ -91734,16 +91734,51 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.App = App;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _rsuite = require("rsuite");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function App() {
+  var _useState = (0, _react.useState)(true),
+      _useState2 = _slicedToArray(_useState, 2),
+      expanded = _useState2[0],
+      setExpanded = _useState2[1];
+
+  var sideBarChanged = function sideBarChanged() {
+    setExpanded(!expanded);
+  };
+
   return _react.default.createElement("div", {
     className: "show-container"
-  }, _react.default.createElement(_rsuite.Container, null, _react.default.createElement(_rsuite.Sidebar, null, "Sidebar"), _react.default.createElement(_rsuite.Container, null, _react.default.createElement(_rsuite.Header, null, "Header"), _react.default.createElement(_rsuite.Content, null, "Content"))));
+  }, _react.default.createElement(_rsuite.Container, null, _react.default.createElement(_rsuite.Sidebar, {
+    collapsible: true
+  }, _react.default.createElement(_rsuite.Sidenav, {
+    expanded: expanded,
+    appearance: "inverse"
+  }, _react.default.createElement(_rsuite.Sidenav.Body, null, _react.default.createElement(_rsuite.Nav, null, _react.default.createElement(_rsuite.Nav.Item, {
+    eventKey: "1",
+    onClick: sideBarChanged,
+    active: true,
+    icon: _react.default.createElement(_rsuite.Icon, {
+      icon: expanded ? "close" : "bars"
+    })
+  }, "Viewer"))))), _react.default.createElement(_rsuite.Container, null, _react.default.createElement(_rsuite.Header, null, "Header"), _react.default.createElement(_rsuite.Content, null, "Content"))));
 }
 },{"react":"n8MK","rsuite":"JnKi"}],"wGC4":[function(require,module,exports) {
 "use strict";
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { Container, Content, Header, Sidebar } from "rsuite";
+import React, { useState } from "react";
+import { Container, Content, Header, Icon, Nav, Sidebar, Sidenav } from "rsuite";
 
 export function App(): JSX.Element {
+    const [expanded, setExpanded] = useState(true);
+
+    const sideBarChanged = () => {
+        setExpanded(!expanded);
+    }
+
     return (
         <div className="show-container">
             <Container>
-                <Sidebar>Sidebar</Sidebar>
+                <Sidebar collapsible>
+                    <Sidenav expanded={expanded} appearance="inverse">
+                        <Sidenav.Body>
+                            <Nav>
+                                <Nav.Item eventKey="1" onClick={sideBarChanged} active icon={<Icon icon={expanded ? "close" : "bars"} />}>
+                                    Viewer
+                                </Nav.Item>
+                            </Nav>
+                        </Sidenav.Body>
+                    </Sidenav>
+                </Sidebar>
                 <Container>
                     <Header>Header</Header>
                     <Content>Content</Content>
